5e763521ff2162f32fbf04ca44cdde727507c671
jeuryink <jeury@inkfnd.com>
2025-11-03 15:22:16 -0500

    chore: update testnet endpoints (#363)

--- packages/indexer-client/src/endpoints.ts
+++ packages/indexer-client/src/endpoints.ts
@@ -3,5 +3,5 @@ import { ChainEnv } from '@nadohq/shared';
 export const INDEXER_CLIENT_ENDPOINTS: Record<ChainEnv, string> = {
   local: 'http://localhost:8000/indexer',
   arbitrum: 'https://archive.prod.vertexprotocol.com/v1',
-  inkTestnet: 'https://archive.test.nado-backend.xyz/v1',
+  inkTestnet: 'https://archive.test.nado.xyz/v1',
 };
--- packages/trigger-client/src/endpoints.ts
+++ packages/trigger-client/src/endpoints.ts
@@ -3,5 +3,5 @@ import { ChainEnv } from '@nadohq/shared';
 export const TRIGGER_CLIENT_ENDPOINTS: Record<ChainEnv, string> = {
   local: 'http://localhost:80/trigger',
   arbitrum: 'https://trigger.prod.vertexprotocol.com/v1',
-  inkTestnet: 'https://trigger.test.nado-backend.xyz/v1',
+  inkTestnet: 'https://trigger.test.nado.xyz/v1',
 };
